b05f123d49df9a71379a217254477037d7ba9f03
Anton Amelekhin <a.amelekhin@outlook.com>
2022-09-24 18:41:29 +0200

    Remove dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,2 @@
-import "dotenv/load.ts";
-
 export const APP_PORT = Deno.env.get("PORT") ? Number(Deno.env.get("PORT")) : 8080;
 export const APP_ORIGIN = Deno.env.get("APP_ORIGIN") ?? "https://example.com/";
