99d26717c135bdc998d8ace59fe3a51308136155
Anton Amelekhin <a.amelekhin@outlook.com>
2022-09-24 15:34:14 +0200

    Add dotenv, oak & tasks

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('short.ly');
+import "dotenv/load.ts";
+import { Application } from "oak";
+
+const app = new Application();
+const port = Number(Deno.env.get("PORT"));
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port });
