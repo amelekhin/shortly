80656fa4e43cd78bdf508bd4793dcb4b9575773a
Anton Amelekhin <a.amelekhin@outlook.com>
2022-09-24 15:39:03 +0200

    Add logger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,21 @@ import { Application } from "oak";
 const app = new Application();
 const port = Number(Deno.env.get("PORT"));
 
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
 app.use((ctx) => {
   ctx.response.body = "Hello World!";
 });
